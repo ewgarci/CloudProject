a40cbbf9e2e320a1b8b045984382b53fa5601bba
Kina Winoto <kwinoto@ucla.edu>
2012-12-13 17:30:08 -0500

    twitter streaming, nltk

---
+++ keyword_extract.py
@@ -0,0 +1,13 @@
+import nltk
+from nltk.collocations import *
+bigram_measures = nltk.collocations.BigramAssocMeasures()
+
+# change this to read in your data
+finder = BigramCollocationFinder.from_words(
+   #nltk.corpus.genesis.words('english-web.txt'))
+
+# only bigrams that appear 3+ times
+finder.apply_freq_filter(3) 
+
+# return the 5 n-grams with the highest PMI
+print finder.nbest(bigram_measures.pmi, 5)  
