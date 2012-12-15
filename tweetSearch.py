6191dab07779036133a775e59cdcfaa30b65e6b1
EC2 Default User <ec2-user@ip-10-28-88-26.ec2.internal>
2012-12-15 10:01:38 +0000

    added cgi script to recieve query from extension. Use http://ec2-50-17-87-12.compute-1.amazonaws.com:8080/cgi-bin/tweetSearch.py?search=<QUERY TERM> to search twitter for a key

---
+++ tweetSearch.py
@@ -0,0 +1,37 @@
+#!/usr/bin/python
+
+import urllib
+import simplejson
+import sys
+import cgitb
+import cgi
+
+from xml.dom.minidom import parseString
+from string import rfind
+from twsearch import searchTweets
+
+cgitb.enable()
+
+form = cgi.FieldStorage()
+
+query = form["search"].value
+
+print "Content-type: text/html\n\n"
+print "<html>"
+
+print "<p>Search Value:", query
+print "<p>"
+
+search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
+# dict is a dictionary created by parsing the search string
+#all tweets are in dict["result"], which is a list of dictionaries
+dict = simplejson.loads(search.read())
+
+for result in dict["results"]:
+	print "<p>"
+	print "*",result["text"].encode('utf-8'),"\n"
+
+#print searchTweets(query)
+
+
+print "</html>"
