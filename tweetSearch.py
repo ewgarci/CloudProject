343d7c9835aede644200647ac9151d190b83d7d8
EC2 Default User <ec2-user@ip-10-28-88-26.ec2.internal>
2012-12-17 04:36:13 +0000

    created functions within twitter get -kina

--- tweetSearch.py
+++ tweetSearch.py
@@ -10,6 +10,21 @@ from xml.dom.minidom import parseString
 from string import rfind
 from twsearch import searchTweets
 
+def getTweets(query):
+	print "<p>Search Value is:", query
+	print "<p>"
+	
+	search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
+	# dict is a dictionary created by parsing the search string
+	#all tweets are in dict["result"], which is a list of dictionaries
+	dict = simplejson.loads(search.read())
+	
+	for result in dict["results"]:
+		print "<p>"
+		print "*",result["text"].encode('utf-8'),"\n"
+	
+	#print searchTweets(query)
+
 cgitb.enable()
 
 form = cgi.FieldStorage()
@@ -19,19 +34,6 @@ query = form["search"].value
 print "Content-type: text/html\n\n"
 print "<html>"
 
-print "<p>Search Value:", query
-print "<p>"
-
-search = urllib.urlopen("http://search.twitter.com/search.json?q="+query)
-# dict is a dictionary created by parsing the search string
-#all tweets are in dict["result"], which is a list of dictionaries
-dict = simplejson.loads(search.read())
-
-for result in dict["results"]:
-	print "<p>"
-	print "*",result["text"].encode('utf-8'),"\n"
-
-#print searchTweets(query)
-
+getTweets(query)
 
 print "</html>"
