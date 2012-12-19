0c88ce628169d745a1383cae3b5d5b79b86d5f69
peter <pbl2108@columbia.edu>
2012-12-19 15:23:49 -0500

    Added code to invoke the cloud server for tweeter feed

--- ChromeExtension/scripts/htk_background_script.js
+++ ChromeExtension/scripts/htk_background_script.js
@@ -1,19 +1,42 @@
 var communicationPort;
+var tab;
 
 /* Listen for connection request */
 chrome.extension.onMessage.addListener(
   function(request, sender, sendResponse) {
-    if (request.openConnection == "true")
-		sendData(sender.tab);
+    if (request.openConnection == "true") {
+		tab = sender.tab;
+		sendRequestToCloud(request.wikiPage);
+		//sendData(tab);
+	}
   });
 
-function sendData(tab) {
+function sendData(tab, data) {
     var tabID = tab.id;
     communicationPort = chrome.tabs.connect(tabID);
     communicationPort.onMessage.addListener(receiveMessage);
-    communicationPort.postMessage({ keyWord : "bomb"});
+    communicationPort.postMessage({ keyWord : "the", content : data });
 }
 
-function receiveMessage(msg) {
 
+
+function receiveMessage(msg) {
+}
+
+function sendRequestToCloud(wikiPage) {
+	var requestUrl = "http://ec2-50-17-87-12.compute-1.amazonaws.com:8080/cgi-bin/tweetSearch.py?" +
+					 "urllink=" + wikiPage;
+	alert("Ping cloud");
+	$.get(
+		requestUrl, 		/* cloud url */
+		{},					/* send nothing to cloud */
+		/* handle response */
+		function(dataReceived) {
+			parseCloudData(dataReceived);
+		});
+		//"json");
+}
+
+function parseCloudData(data) {
+	sendData(tab, data);
 }
--- ChromeExtension/scripts/htk_content_script.js
+++ ChromeExtension/scripts/htk_content_script.js
@@ -4,7 +4,8 @@
 var communicationPort = null;
 
 /* Request a connection */
-chrome.extension.sendMessage({ openConnection: "true", wikiPage : "wikiPageId"});
+	alert(document.URL);
+chrome.extension.sendMessage({ openConnection: "true", wikiPage : document.URL});
 
 function sendMessageBackground() {
 	communicationPort.postMessage(
@@ -18,7 +19,7 @@ function sendMessageBackground() {
 
 function receiveMessageBackground(msg) {
 	var keyWord = msg.keyWord;
-	appendContent(msg.keyWord);
+	appendContent(msg.keyWord, msg.content);
 }
 
 chrome.extension.onConnect.addListener(function(thePort) {
@@ -34,7 +35,7 @@ function markWord(str) {
 	return "<a class='ourKeyWord' title='some text'>" + str + "<\/a>";
 }
 
-function appendContent(keyWord) {
+function appendContent(keyWord, data) {
 	window.setTimeout(function(){
 		var regex = keyWord;
 		var re = new RegExp(regex, "gi");
@@ -44,48 +45,12 @@ function appendContent(keyWord) {
 		
 		/* Add tooltip that will show the twitter data on hover. */
 		$( ".ourKeyWord" ).tooltip({ 
-										content: function() {return parseHTML();} 
+										content: function() {return parseHTML(data);} 
 									});
 	}, 0);	
 }
 
-function parseHTML() {
-	return "<div style='color: Red; width:300px; height:100px;'>An awesome html content.</div>";
-}
-
-// Copyright (c) 2012 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
-var req = new XMLHttpRequest();
-req.open(
-    "GET",
-    "http://api.flickr.com/services/rest/?" +
-        "method=flickr.photos.search&" +
-        "api_key=90485e931f687a9b9c2a66bf58a3861a&" +
-        "text=hello%20world&" +
-        "safe_search=1&" +  // 1 is "safe"
-        "content_type=1&" +  // 1 is "photos only"
-        "sort=relevance&" +  // another good one is "interestingness-desc"
-        "per_page=20",
-    true);
-req.onload = showPhotos;
-req.send(null);
-
-function showPhotos() {
-  var photos = req.responseXML.getElementsByTagName("photo");
-
-  for (var i = 0, photo; photo = photos[i]; i++) {
-    var img = document.createElement("image");
-    img.src = constructImageURL(photo);
-    document.body.appendChild(img);
-  }
-}
-
-// See: http://www.flickr.com/services/api/misc.urls.html
-function constructImageURL(photo) {
-  return "http://farm" + photo.getAttribute("farm") +
-      ".static.flickr.com/" + photo.getAttribute("server") +
-      "/" + photo.getAttribute("id") +
-      "_" + photo.getAttribute("secret") +
-      "_s.jpg";
+function parseHTML(data) {
+	return data;
+	//return "<div style='color: Red; width:300px; height:100px;'>An awesome html content.</div>";
 }
